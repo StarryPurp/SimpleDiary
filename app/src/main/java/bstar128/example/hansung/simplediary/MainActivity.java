adcf9a6ea591c3e8674174a8fd5fc3d240f5b964
StarryPurp <starbloom128@gmail.com>
2017-05-30 20:28:19 +0900

    #2 View

--- app/src/main/java/bstar128/example/hansung/simplediary/MainActivity.java
+++ app/src/main/java/bstar128/example/hansung/simplediary/MainActivity.java
@@ -2,12 +2,21 @@ package bstar128.example.hansung.simplediary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    DatePicker date;
+    EditText edit;
+    Button but;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        date=(DatePicker)findViewById(R.id.datep);
+        edit=(EditText)findViewById(R.id.edit_1);
+        but=(Button)findViewById(R.id.but_1);
     }
 }
